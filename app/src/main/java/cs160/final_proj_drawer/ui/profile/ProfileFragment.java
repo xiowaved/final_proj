9c922bdc360da438ca038309f4457cc1ed4da454
Vron Vance <xiowaved@berkeley.edu>
2019-11-20 12:13:59 -0800

    Changed everything to profile.

--- app/src/main/java/cs160/final_proj_drawer/ui/profile/ProfileFragment.java
+++ app/src/main/java/cs160/final_proj_drawer/ui/profile/ProfileFragment.java
@@ -10,20 +10,21 @@ import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProviders;
 import cs160.final_proj_drawer.R;
 
 public class ProfileFragment extends Fragment {
 
-    private ProfileViewModel galleryViewModel;
+    private ProfileViewModel profileViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
+        profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        final TextView textView = root.findViewById(R.id.text_profile);
+        profileViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
