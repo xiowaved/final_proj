17d836f20a5968df82014c78edd8425ee039318f
Vron Vance <xiowaved@berkeley.edu>
2019-11-20 11:14:46 -0800

    drawer activity default.

---
+++ app/src/main/java/cs160/final_proj_drawer/ui/send/SendViewModel.java
@@ -0,0 +1,19 @@
+package cs160.final_proj_drawer.ui.send;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SendViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SendViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is send fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
