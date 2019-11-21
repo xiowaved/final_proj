0b1377ce45d20893b08692886119e16d59e15c31
Colby <colbymccann@berkeley.edu>
2019-11-20 17:52:41 -0800

    Added firebase

--- app/src/main/java/cs160/final_proj_drawer/LoginActivity.java
+++ app/src/main/java/cs160/final_proj_drawer/LoginActivity.java
@@ -2,6 +2,120 @@ package cs160.final_proj_drawer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
+import androidx.annotation.NonNull;
+
+
+        import android.content.Context;
+        import android.content.Intent;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.EditText;
+        import android.widget.TextView;
+
+        import com.google.android.gms.tasks.OnFailureListener;
+        import com.google.android.gms.tasks.OnSuccessListener;
+        import com.google.firebase.storage.FirebaseStorage;
+        import com.google.firebase.storage.StorageReference;
+        import com.google.firebase.storage.UploadTask;
+
+        import java.io.BufferedReader;
+        import java.io.File;
+        import java.io.FileNotFoundException;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.io.OutputStreamWriter;
+        import java.lang.reflect.Array;
+        import java.net.URI;
+        import com.google.android.material.snackbar.Snackbar;
+//delete later: Hi I'm Vron trying to figure out wth is happening
+
 public class LoginActivity extends AppCompatActivity {
+
+    private StorageReference mStorageRef;
+    Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        context = getApplicationContext();
+
+    }
+
+    public void saveText (View view) {
+        EditText username = findViewById(R.id.editText);
+        String user = username.getText().toString();
+        writeToFile(user, context);
+        Uri config = Uri.fromFile(new File("config.txt"));
+        StorageReference riversRef = mStorageRef.child("config.txt");
+        riversRef.putFile(config)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Uri downloadUrl = taskSnapshot.getUploadSessionUri();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+        Snackbar.make(view, user, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+        //Intent intent = new Intent(this, MainActivity.class);
+        //startActivity(intent);
+
+    }
+    private void writeToFile(String data,Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+
+
+    private String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("config.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
 
 }
