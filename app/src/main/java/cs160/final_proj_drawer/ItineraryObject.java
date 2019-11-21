6ac628b67ddab4a7d1845c5dff0d1a70fce9be5f
Dzanela Muminovic <Dzanela@Dzanelas-MacBook-Pro.local>
2019-11-20 18:22:25 -0800

    pushing stop class

--- app/src/main/java/cs160/final_proj_drawer/ItineraryObject.java
+++ app/src/main/java/cs160/final_proj_drawer/ItineraryObject.java
@@ -9,7 +9,7 @@ public class ItineraryObject {
      private int numLikes;
      private String coverPhoto;
      private int numStops;
-     private List<String> stops;
+     private List<Stop> stops;
      private List<String> tags;
      private List<String> access;
 
@@ -43,7 +43,7 @@ public class ItineraryObject {
         return this.numStops;
     }
 
-    public List<String> getStops() {
+    public List<Stop> getStops() {
         return this.stops;
     }
 
@@ -58,34 +58,3 @@ public class ItineraryObject {
 }
 
 
-class Stop {
-     private List<String> photos;
-     private String name;
-     private String location;
-     private int index;
-
-
-     public void Stop(List<String> photos, String name, String location, int index) {
-         // Create a Stop instance
-         this.photos = photos;
-         this.name = name;
-         this.location = location;
-         this.index = index;
-
-     }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String getLocation() {
-        return this.location;
-    }
-    public List<String> getPhotos() {
-        return this.photos;
-    }
-
-    public int getIndex() {
-         return this.index;
-    }
-}
