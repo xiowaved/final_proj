829f5e77339a28f1915d4a5b34db8da8c0120bc9
Dzanela Muminovic <Dzanela@Dzanelas-MacBook-Pro.local>
2019-11-22 15:25:35 -0800

    pushing home screen changes - 2 search bars'

--- app/src/main/java/cs160/final_proj_drawer/ui/home/HomeFragment.java
+++ app/src/main/java/cs160/final_proj_drawer/ui/home/HomeFragment.java
@@ -1,9 +1,13 @@
 package cs160.final_proj_drawer.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -16,19 +20,87 @@ import cs160.final_proj_drawer.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private Button searchButton;
 
+    // top search bar -- query itinerary tags
+    private EditText tagSearchBar;
+    // lower search bar, appears after clicking inside top one (see Yelp as example); enter location
+    private EditText locationSearchBar;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        final View root = inflater.inflate(R.layout.fragment_home, container, false);
+        tagSearchBar = root.findViewById(R.id.tagSearchBar);
+        searchButton = root.findViewById(R.id.searchButton);
+
+
+        tagSearchBar.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onFocusChange(View view, boolean hasFocus) {
+                Log.i("user clicked", "TESTING CLICK");
+                locationSearchBar = root.findViewById(R.id.locationSearchBar);
+                locationSearchBar.setVisibility(View.VISIBLE);
             }
         });
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("user searched", "TESTING BUTTON");
+                queryCallback(locationSearchBar.getText().toString() + " " + tagSearchBar.getText().toString());
+            }
+        });
+//        tagSearchBar.setOnEd(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                queryCallback(query);
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                return false;
+//            }
+//        });
+
+        // THIS OVERRIDEN FUNCTION IS NOT BEING CALLED WHEN CLICK INSIDE SEARCHVIEW
+//        tagSearchBar.setOnSearchClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.i("user query", "TESTING");
+//                locationSearchBar = root.findViewById(R.id.locationSearchBar);
+//                locationSearchBar.setVisibility(View.VISIBLE);
+//            }
+//        });
+
+
+
+
+
+
         return root;
+    }
+
+
+    public void queryCallback(String query) {
+        /**
+         * CB function when user submits a query to search for an itinerary on the home page.
+         * If there is no text in the query yet, display the default home page results for the
+         * user's current location
+         */
+//        String currentQuery = tagSearchBar.getQuery().toString();
+        if (query == null) {
+            // search bar query is not filled in, which means user just opened the home page
+            // display itineraries in current location
+            // JSON request matching itineraries in current user location
+        }
+        else {
+            // search bar has a query in it, search for itineraries matching this one
+            // JSON request matching 1) location in query and 2) tags in search
+            Log.i("user searched", query);
+
+        }
+
     }
 }
