a1c6eddbf791a9649ca129dbee91141288443b7b
Dzanela Muminovic <Dzanela@Dzanelas-MacBook-Pro.local>
2019-11-24 21:16:53 -0800

    cleaned up login page

--- app/src/main/java/cs160/final_proj_drawer/ui/home/HomeFragment.java
+++ app/src/main/java/cs160/final_proj_drawer/ui/home/HomeFragment.java
@@ -17,6 +17,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
