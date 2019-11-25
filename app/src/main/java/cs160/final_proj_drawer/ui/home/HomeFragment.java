86d888e291fc4535c5e5617f1a488d640f8d2a8f
Vron Vance <xiowaved@berkeley.edu>
2019-11-24 17:37:00 -0800

    another resize.

--- app/src/main/java/cs160/final_proj_drawer/ui/home/HomeFragment.java
+++ app/src/main/java/cs160/final_proj_drawer/ui/home/HomeFragment.java
@@ -8,16 +8,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.SearchView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
