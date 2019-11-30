eab039c3fd34e709f54c2a1181306be6fa125770
Vron Vance <xiowaved@berkeley.edu>
2019-11-30 12:13:06 -0800

    added file for single itin layout.

--- app/src/main/java/cs160/final_proj_drawer/ui/itin/DisplaySingleItinFragment.java
+++ app/src/main/java/cs160/final_proj_drawer/ui/itin/DisplaySingleItinFragment.java
@@ -1,4 +1,18 @@
 package cs160.final_proj_drawer.ui.itin;
 
-public class DisplaySingleItinFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import cs160.final_proj_drawer.R;
+
+public class DisplaySingleItinFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_display_itin, container, false);
+        return root;
+    }
 }
