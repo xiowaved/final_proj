d9422dc1f8756829de79c3aff644a6db8431ab87
Dzanela Muminovic <Dzanela@Dzanelas-MacBook-Pro.local>
2019-12-01 12:39:24 -0800

    added submit button with onClickListener to reviewFrag

--- app/src/main/java/cs160/final_proj_drawer/ui/create/ReviewItineraryFragment.java
+++ app/src/main/java/cs160/final_proj_drawer/ui/create/ReviewItineraryFragment.java
@@ -11,8 +11,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -31,12 +33,24 @@ public class ReviewItineraryFragment extends Fragment {
     private StopAdapter stopAdapter;
     private TextView errorMsg;
     private ItineraryObject itin;
+    private Button submit;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         final View root = inflater.inflate(R.layout.fragment_review_itinerary, container, false);
 
+        // get UI stuff
         errorMsg = (TextView) root.findViewById(R.id.errorMsg);
+        submit = (Button) root.findViewById(R.id.submit);
+        // set listener for button when user submits itin
+        submit.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  Toast toast = Toast.makeText(getContext(), "clicked on submit itin",
+                          Toast.LENGTH_SHORT);
+                  toast.show();
+              }
+          });
         //recycler view setup
         stops = (RecyclerView) root.findViewById(R.id.stops);
         final LinearLayoutManager stopsLayoutManager = new LinearLayoutManager(getActivity());
